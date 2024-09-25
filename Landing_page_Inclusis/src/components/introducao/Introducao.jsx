d4230d90f06dc73046729c157e0495c6cd5f83af
Diogobq7 <diogo.queiroz@germinare.org.br>
2024-09-25 10:36:11 -0300

    Novas atualizações da landing page

--- Landing_page_Inclusis/src/components/introducao/Introducao.jsx
+++ Landing_page_Inclusis/src/components/introducao/Introducao.jsx
@@ -1,8 +1,8 @@
-import "./Introducao.module.css";
+import style from "./Introducao.module.css";
 
 function Introducao() {
     return (
-        <img src="src\assets\WhatsApp Image 2024-09-19 at 10.57.23 AM.jpeg" alt="imagem de introdução" />
+        <img src="src\assets\WhatsApp Image 2024-09-19 at 10.57.23 AM.jpeg" alt="imagem de introdução" id={style.imagem}/>
     )
 }
 
