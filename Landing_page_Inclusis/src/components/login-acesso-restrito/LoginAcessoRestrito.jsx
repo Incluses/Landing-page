0cd13f698fb57a99ee63e031a483d4be4748ccb7
IRenatoBarros <renatofilho9722@gmail.com>
2024-09-28 16:23:32 -0300

    subindo as alterações de responsividade

--- Landing_page_Inclusis/src/components/login-acesso-restrito/LoginAcessoRestrito.jsx
+++ Landing_page_Inclusis/src/components/login-acesso-restrito/LoginAcessoRestrito.jsx
@@ -17,7 +17,7 @@ function loginAcessoRestrito(){
         //     window.alert("não foi")
         // }
     };
-    
+
     const navigate = useNavigate();
 
     const goToGrafic = () => {
@@ -30,17 +30,23 @@ function loginAcessoRestrito(){
     <div className={style.geral}>
         <img src={imagemFundo} className={style.img_fundo}></img>
         <div className={style.card}>
-            <h1 id={style.titulo}>Incluses</h1>
-            <h2 id={style.subtitulo}>Acesso Restrito</h2>
+            <div className={style.textos}>
+                <h1 id={style.titulo}>Incluses</h1>
+                <h2 id={style.subtitulo}>Acesso Restrito</h2>
+            </div>
             <div id={style.inputs}>
                 <InputTexto onChange={(e) => setEmail(e.target.value)} placeholder="Ex: Exemple@gmail.com" text="Digite seu login:" type="text" id="inputEmail"></InputTexto>
                 <InputTexto placeholder="Ex: *********" text="Digite sua senha:" type="password"></InputTexto>
             </div>
             <Link text="Esqueci minha senha" className={style.senha}></Link>
-             <BotaoPrincipal onClick={() => {goToGrafic()}} text="Entrar" backgroundColor="blue" marginTop="13%" marginBotton="30%"></BotaoPrincipal> 
-            <p id={style.texto1}>Ao continuar, você concorda com a</p>
-            <p id={style.texto2}>politica de privacidade e Termo de uso.</p>
+            <div className={style.botao}>
+                <BotaoPrincipal onClick={() => {goToGrafic()}} text="Entrar" backgroundColor="blue" marginTop="13%" marginBotton="30%"></BotaoPrincipal>
+            </div>
         </div>
+        <div className={style.termos}>
+                <p id={style.texto1}>Ao continuar, você concorda com a</p>
+                <p id={style.texto2}>politica de privacidade e Termo de uso.</p>
+            </div>
 
     </div>)
 }
