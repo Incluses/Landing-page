7cb826d2691c6dd7b16730c3a8c667db30c83048
IRenatoBarros <renatofilho9722@gmail.com>
2024-09-29 19:44:04 -0300

    alterações de responsividade

--- Landing_page_Inclusis/src/components/input-texto/InputTexto.jsx
+++ Landing_page_Inclusis/src/components/input-texto/InputTexto.jsx
@@ -1,7 +1,7 @@
 import style from "./InputTexto.module.css"
 
 function InputTexto({placeholder,text,type, onChange}){
-    return(<div>
+    return(<div className={style.geral}>
         <p id={style.texto}>{text}</p>
         <input onChange={onChange} type={type} placeholder={placeholder} className={style.input}></input>
     </div>)
