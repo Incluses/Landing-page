eabf63dc8930c41261b50f935d162e618de0d9ad
Diogobq7 <diogo.queiroz@germinare.org.br>
2024-10-02 20:45:38 -0300

    Enviando a landing page com a imagem

--- Landing_page_Inclusis/src/components/introducao/Introducao.jsx
+++ Landing_page_Inclusis/src/components/introducao/Introducao.jsx
@@ -1,8 +1,19 @@
 import style from "./Introducao.module.css";
+import Button from 'react-bootstrap/Button';
+
 
 function Introducao() {
     return (
-        <img src="src\assets\WhatsApp Image 2024-09-19 at 10.57.23 AM.jpeg" alt="imagem de introdução" id={style.imagem}/>
+        <div>
+            <img src="src/assets/Group 167.png" alt="imagem de pessoas" id={style.imgPessoas}/>
+            <Button variant="primary" id={style.botao}>Sobre nós</Button>{' '}
+            <p id={style.maisOportu}>+ mais oportunidades oferecidas pra você</p>
+            <p id={style.tituloIntrodu}><span>Inclus<span>es</span></span>, a oportunidade</p>
+            <p id={style.tituloIntrodu2}>que <span>você</span> precisa para aprender,</p>
+            <p id={style.tituloIntrodu3}>empreender, trabalhar e</p>
+            <p id={style.tituloIntrodu4}>crescer</p>
+            <img src="src/assets/Group 168.png" alt="imagem de introdução" id={style.imagem}/>
+            </div>
     )
 }
 
