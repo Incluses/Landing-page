5012008da59866443f02bd62009b11cfda1bdf0d
Diogobq7 <diogo.queiroz@germinare.org.br>
2024-10-03 09:22:42 -0300

    Adicionando mais alterações à Landing Page

--- Landing_page_Inclusis/src/components/introducao/Introducao.jsx
+++ Landing_page_Inclusis/src/components/introducao/Introducao.jsx
@@ -7,12 +7,18 @@ function Introducao() {
         <div>
             <img src="src/assets/Group 167.png" alt="imagem de pessoas" id={style.imgPessoas}/>
             <Button variant="primary" id={style.botao}>Sobre nós</Button>{' '}
+
             <p id={style.maisOportu}>+ mais oportunidades oferecidas pra você</p>
             <p id={style.tituloIntrodu}><span>Inclus<span>es</span></span>, a oportunidade</p>
             <p id={style.tituloIntrodu2}>que <span>você</span> precisa para aprender,</p>
             <p id={style.tituloIntrodu3}>empreender, trabalhar e</p>
             <p id={style.tituloIntrodu4}>crescer</p>
+
             <img src="src/assets/Group 168.png" alt="imagem de introdução" id={style.imagem}/>
+            
+            <div id={style.divFacaCursos}>
+                <p id={style.facaCursos}>Faça cursos e aprenda a gerenciar seu dinheiro para que o empoderamento não seja só moral</p>
+            </div>
             </div>
     )
 }
