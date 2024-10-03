14f41dd64c9ec6cb93e30628fb58327436fbb164
Diogobq7 <diogo.queiroz@germinare.org.br>
2024-10-03 12:00:36 -0300

    Adicionando as mudanças de position: absolute; para Display Flex na introdução da Landing Page

--- Landing_page_Inclusis/src/components/introducao/Introducao.jsx
+++ Landing_page_Inclusis/src/components/introducao/Introducao.jsx
@@ -4,22 +4,23 @@ import Button from 'react-bootstrap/Button';
 
 function Introducao() {
     return (
-        <div>
-            <img src="src/assets/Group 167.png" alt="imagem de pessoas" id={style.imgPessoas}/>
-            <Button variant="primary" id={style.botao}>Sobre nós</Button>{' '}
-
-            <p id={style.maisOportu}>+ mais oportunidades oferecidas pra você</p>
-            <p id={style.tituloIntrodu}><span>Inclus<span>es</span></span>, a oportunidade</p>
-            <p id={style.tituloIntrodu2}>que <span>você</span> precisa para aprender,</p>
-            <p id={style.tituloIntrodu3}>empreender, trabalhar e</p>
-            <p id={style.tituloIntrodu4}>crescer</p>
-
-            <img src="src/assets/Group 168.png" alt="imagem de introdução" id={style.imagem}/>
+        <>
+            <div id={style.imagem}>
+                <div id={style.introdu}>
+                    <div id={style.maisOportu}>
+                        <img src="src\assets\Group 167.png" alt="imagem de pessoas" id={style.imgPessoas}/>
+                        <p>+ mais oportunidades oferecidas pra você</p>
+                    </div>
+                        <p></p>
+                        <p><span>Inclus<span>es</span></span>, a oportunidade <br />que <span>você</span> precisa para aprender, <br /> empreender, trabalhar e <br /> crescer</p>
+                        <Button variant="primary" id={style.botao}>Sobre nós</Button>{' '}
+                </div>
+            </div>
             
             <div id={style.divFacaCursos}>
                 <p id={style.facaCursos}>Faça cursos e aprenda a gerenciar seu dinheiro para que o empoderamento não seja só moral</p>
             </div>
-            </div>
+        </>
     )
 }
 
