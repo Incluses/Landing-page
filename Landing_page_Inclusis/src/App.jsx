46c227b65950cbc9c16e1772098b65c0270c9131
BrunaCCardoso <bruna.cardoso@germinare.org.br>
2024-10-06 00:35:17 -0300

    Adicionando novos componentes à Landing Page.

--- Landing_page_Inclusis/src/App.jsx
+++ Landing_page_Inclusis/src/App.jsx
@@ -1,6 +1,7 @@
 import NavScrollExample from "./components/navbar/NavBar.jsx";
 import Introducao from "./components/introducao/Introducao.jsx"
 import Beneficios from "./components/beneficios/Beneficios.jsx";
+import Conteudo from "./components/conteudo/Conteudo.jsx";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
     <NavScrollExample />
     <Introducao />
     <Beneficios />
+    <Conteudo />
     </>
   )
 }
---
+++ Landing_page_Inclusis/src/components/conteudo/Conteudo.jsx
@@ -0,0 +1,12 @@
+import Botao from "./components/botao/Botao.jsx";
+import BotaoDois from "./components/botao/BotaoDois.jsx"
+import style from './Conteudo.module.css';
+
+function Conteudo(){
+    return (
+        <>
+           <Botao /> 
+           <BotaoDois />
+        </>
+    )
+}
