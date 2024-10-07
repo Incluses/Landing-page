6027b98ea615d1353162f1d5a58686652ee1c38f
Diogobq7 <diogo.queiroz@germinare.org.br>
2024-10-07 12:45:31 -0300

    Adicionando conteúdo na área de Empresa no componente Conteudo

--- Landing_page_Inclusis/src/components/conteudo/Conteudo.jsx
+++ Landing_page_Inclusis/src/components/conteudo/Conteudo.jsx
@@ -33,6 +33,16 @@ function Conteudo() {
                     {botaoSelecionado === 'empresa' && (
                         <div className={style.divMaeEmpresa}>
                             <img src="src\assets\Group 191.png" alt="fundo_roxo" id={style.imagemOrganizacao} />
+                            <div id={style.conteudo}>
+                                <p className={style.pMae}><p className={style.nums}>1</p>Oportunidades de emprego</p>
+                                <p className={style.conteudoFilho}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed <br /> do eiusmod temporqua.</p>
+
+                                <p id={style.pMaeDois}><p className={style.nums}>2</p>Network</p>
+                                <p className={style.conteudoFilho}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed <br /> do eiusmod temporqua.</p>
+
+                                <p className={style.pMae}><p className={style.nums}>3</p>Mais conforto e tranparência</p>
+                                <p className={style.conteudoFilho}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed <br /> do eiusmod temporqua.</p>
+                            </div>
                         </div>
                     )}
                 </div>
