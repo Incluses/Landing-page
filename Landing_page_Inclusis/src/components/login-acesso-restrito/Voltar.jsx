4f4e3fb1ec50cf6c1e3e764f68772cd6f8776fa1
BrunaCCardoso <bruna.cardoso@germinare.org.br>
2024-10-10 21:40:59 -0300

    Finalizando tela de Login

--- Landing_page_Inclusis/src/components/login-acesso-restrito/Voltar.jsx
+++ Landing_page_Inclusis/src/components/login-acesso-restrito/Voltar.jsx
@@ -1,11 +1,18 @@
 import Button from 'react-bootstrap/Button';
 import style from './Voltar.module.css';
+import { useNavigate } from 'react-router-dom';
 
 function Voltar({ onClick }) {
+    const navigate = useNavigate();
     let seta = '<';
+
+    function handleVoltar() {
+        navigate(-1); 
+    }
+
     return (
         <>
-            <Button onClick={onClick}  className={style.botaoDois}variant='primary'>{seta} voltar</Button>{' '}
+            <Button onClick={handleVoltar}  className={style.botaoDois}variant='primary'>{seta} Voltar</Button>{' '}
         </>
     )
 }
