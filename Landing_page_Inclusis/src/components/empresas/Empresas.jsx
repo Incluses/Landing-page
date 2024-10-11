b5bf0769a9d06012a7e5d15bbe569f021d07c4dc
BrunaCCardoso <bruna.cardoso@germinare.org.br>
2024-10-11 13:55:57 -0300

    Fazendo novo componente Empresas

---
+++ Landing_page_Inclusis/src/components/empresas/Empresas.jsx
@@ -0,0 +1,15 @@
+import style from './Empresas.module.css';
+
+function Empresas() {
+    return (
+        <div id={style.containerEmpresas}>
+            <div id={style.divEmpresasCausas}>
+                <p id={style.empresasCausas}>Conheça as empresas das causas</p>
+            </div>
+        </div>
+        
+    );
+}
+
+
+export default Empresas;
--- Landing_page_Inclusis/src/components/main/main.jsx
+++ Landing_page_Inclusis/src/components/main/main.jsx
@@ -2,6 +2,7 @@ import NavScrollExample from "../navbar/NavBar.jsx";
 import Introducao from "../introducao/Introducao.jsx"
 import Beneficios from "../beneficios/Beneficios.jsx";
 import Conteudo from "../conteudo/Conteudo.jsx";
+import Empresas from "../empresas/Empresas.jsx";
 import style from "./Main.module.css"
 
 function Main(){
@@ -11,6 +12,7 @@ function Main(){
             <Introducao />
             <Beneficios />
             <Conteudo />
+            <Empresas />
         </div>
     )
 }
