befe2573623424f227f25d9d26e855e68437e824
Diogobq7 <diogo.queiroz@germinare.org.br>
2024-10-11 15:01:45 -0300

    Adicionsndo itens no componente Empresas

--- Landing_page_Inclusis/src/components/empresas/Empresas.jsx
+++ Landing_page_Inclusis/src/components/empresas/Empresas.jsx
@@ -5,6 +5,10 @@ function Empresas() {
         <div id={style.containerEmpresas}>
             <div id={style.divEmpresasCausas}>
                 <p id={style.empresasCausas}>Conheça as empresas das causas</p>
+                <p id={style.inclusesTexto}>Incluses é o app que só aceita <br /> 
+                empresas que visam a inclusão e <br />
+                a diversidade, assim como a </p>
+                <img src="src\assets\Group 193.png" alt="Logo da Diversis" />
             </div>
         </div>
         
