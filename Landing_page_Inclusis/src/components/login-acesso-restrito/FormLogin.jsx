1a14c526b2fdb0afb90dd6b884f2820458dc82bf
Diogobq7 <diogo.queiroz@germinare.org.br>
2024-10-13 22:11:58 -0300

    Fazendo alterações na área de login de ADM

--- Landing_page_Inclusis/src/components/login-acesso-restrito/FormLogin.jsx
+++ Landing_page_Inclusis/src/components/login-acesso-restrito/FormLogin.jsx
@@ -1,81 +1,26 @@
-import React, { useState } from "react";
-import { useForm } from "react-hook-form";
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
-import style from "./FormLogin.module.css";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import style from './FormLogin.module.css'
 
-const FormLogin = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-  const [submitted, setSubmitted] = useState(false);
-
-  const onSubmit = (data) => {
-    // Aqui você implementaria a lógica de envio dos dados para o servidor
-    console.log(data);
-    setSubmitted(true);
-  };
-
-  const regexNome =
-    /^adm((?=.[A-Z]{1,})?|(?=.[a-z]{1,}))(?=.\d{1,})?(?=.[\!\@\#\$\%\^\&\*\(\)\_\-\+\=\[\]\{\}\|\;\:\'\"\,\.\<\>\/\?])?.{3,}/;
-  const regexSenha =
-    /^(?=.[A-Z])(?=.[a-z])(?=.[áàâãéèêíïóôõöú])?(?=.[\!\@\#\$\%\^\&\*\(\)\_\-\+\=\[\]\{\}\|\;\:\'\"\,\.\<\>\/\?]).{8,}$/;
-
+function FormLogin() {
   return (
     <div>
-      <h1 id={style.titulo}>
-        Inclu<span id={style.tituloDiferente}>ses</span>
-      </h1>
+      <h1 id={style.titulo}>Inclu<span id={style.tituloDiferente}>ses</span></h1>
       <hr id={style.linha} />
       <p id={style.textoLogin}>Acesso Restrito</p>
-      <Form id={style.form} onSubmit={handleSubmit(onSubmit)}>
+      <Form id={style.form}>
+        <Form.Label id={style.textoMae}>Administrador: </Form.Label>
+        <Form.Control id={style.textoFilho} type="text" placeholder="Ex: adm123" />
         <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label id={style.textoMae}>Administrador: </Form.Label>
-          <Form.Control
-            id={style.textoFilho}
-            type="text"
-            placeholder="Ex: adm123"
-            {...register("nome", {
-              required: "Campo obrigatório",
-              pattern: {
-                value: regexNome,
-                message: "Nome de usuário inválido",
-              },
-            })}
-          />
-          {errors.nome && <span>{errors.nome.message}</span>}
         </Form.Group>
-
         <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label id={style.textoMae}>Digite a senha: </Form.Label>
-          <Form.Control
-            id={style.textoFilho}
-            type="password"
-            placeholder="Senha"
-            {...register("senha", {
-              required: "Campo obrigatório",
-              pattern: {
-                value: regexSenha,
-                message: "Senha inválida",
-              },
-            })}
-          />
-          {errors.senha && <span>{errors.senha.message}</span>}
+        <Form.Label id={style.textoMae}>Digite a senha: </Form.Label>
+        <Form.Control id={style.textoFilho} type="password" placeholder="Senha" />
         </Form.Group>
-
-        <Button
-          variant="primary"
-          type="submit"
-          id={style.botao}
-          disabled={submitted}
-        >
-          Entrar
-        </Button>
+        <Button variant="primary" type="submit" id={style.botao}>Entrar</Button>
       </Form>
     </div>
   );
-};
+}
 
 export default FormLogin;
