f8c9dc38d701c812a373600810e1e45d2700768d
IRenatoBarros <renatofilho9722@gmail.com>
2024-10-16 07:13:47 -0300

    subindo power BI

--- Landing_page_Inclusis/src/components/grafic/Grafic.jsx
+++ Landing_page_Inclusis/src/components/grafic/Grafic.jsx
@@ -7,6 +7,7 @@ function Grafic(){
     return(
         <div className={style.geral}>
             <Navbar color1="#2B46D3"></Navbar>
+            <iframe title="bi_incluses" width="100%" height="631.25" src="https://app.powerbi.com/reportEmbed?reportId=7dc4c68a-be3b-4e8d-9b09-ac92f749ba3f&autoAuth=true&ctid=b148f14c-2397-402c-ab6a-1b4711177ac0" frameborder="0" allowFullScreen="true"></iframe>
         </div>
     )
 }
