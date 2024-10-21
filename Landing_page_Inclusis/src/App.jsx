ba5f05a2769754ca5e0cd31ef7f757f06f233e0a
IRenatoBarros <renatofilho9722@gmail.com>
2024-10-21 12:48:31 -0300

    subindo o componente da pagina intermediaria não completo

--- Landing_page_Inclusis/src/App.jsx
+++ Landing_page_Inclusis/src/App.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginAcessoRestrito from "./components/login-acesso-restrito/LoginAcessoRestrito.jsx"
 import Grafic  from "./components/grafic/Grafic.jsx";
 import Main from "./components/main/main.jsx"
+import TelaIntermediaria from "./components/Tela-intermediaria/Tela-intermediaria.jsx";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Main/>}/>
+        <Route path="/intermediaria" element={<TelaIntermediaria/>}></Route>
         <Route path="/login" element={<LoginAcessoRestrito/>}/>
       </Routes>
     </Router>
