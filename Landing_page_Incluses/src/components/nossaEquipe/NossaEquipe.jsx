bd27fca6f76c4b4b3c909abc174fc5226190d6f2
BrunaCCardoso <bruna.cardoso@germinare.org.br>
2024-11-04 11:32:37 -0300

    Arrumando posicionamento dos elementos

--- Landing_page_Incluses/src/components/nossaEquipe/NossaEquipe.jsx
+++ Landing_page_Incluses/src/components/nossaEquipe/NossaEquipe.jsx
@@ -2,21 +2,14 @@ import style from "./NossaEquipe.module.css";
 
 function NossaEquipe() {
   return (
-    <>
-      <img
-        src="src/assets/Rectangle 467 (1).png"
-        alt="retangulo_roxo"
-        id={style.retanguloRoxo}
-      />
-
-      <div id={style.nossaEquipe}>
+    <div id={style.divMae}>
         <img src="src\assets\Group 235.png" alt="" id={style.imgBloco} />
         <div id={style.textoPrincipal}>
           <p id={style.titulo}>Nossas crenças</p>
           <p className={style.texto}>
             Diversis é uma empresa comprometida em transformar vidas. <br />
             Nosso foco está em promover a inclusão e criar oportunidades <br />
-            reais para pessoas.{" "}
+            reais para pessoas.
           </p>
           <p className={style.texto}>
             Acreditamos no poder da diversidade como um motor para a <br />
@@ -24,8 +17,7 @@ function NossaEquipe() {
             essa diversidade em todos os ambientes em que atuamos.
           </p>
         </div>
-      </div>
-    </>
+    </div>
   );
 }
 
