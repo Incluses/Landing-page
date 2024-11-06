a9402ef33345b2d79b175fdbedfbc0b89b52ed8b
IRenatoBarros <renatofilho9722@gmail.com>
2024-11-06 09:20:21 -0300

    arrumando o problema do press K, e adicionando o novo iframe

--- Landing_page_Incluses/src/components/grafic/Grafic.jsx
+++ Landing_page_Incluses/src/components/grafic/Grafic.jsx
@@ -31,20 +31,26 @@ function Grafic(){
 
   const handleTabPress = (event) => {
     if (event.key === 'k' || event.key === 'K') {
-      location.reload()
       trocarCor()
-      alternarImagem()
+      console.log('passou')
+    }
+  };
+
+  const handleTabPress2 = (event) => {
+    if (event.key === 'k' || event.key === 'K') {
+      trocarCor()
       console.log('passou')
     }
   };
 
   useEffect(() => {
     window.addEventListener('keydown', handleTabPress);
+
     
     return () => {
-      window.removeEventListener('keydown', handleTabPress);
+      
     };
-  }, []);
+  }, [backgroundColor]);
 
 
     return(
@@ -52,6 +58,7 @@ function Grafic(){
             <Navbar color1="#2B46D3" color2="#2B46D3" trocarCor={trocarCor}></Navbar>
             <div id={style.main}>
             <iframe title="bi_incluses_desktop" width="80%" height="673.5" src="https://app.powerbi.com/view?r=eyJrIjoiYjlkMjgwOTEtYmQwMC00ZWUxLTk1YzMtMzY3ZWFiMmM4M2I4IiwidCI6ImIxNDhmMTRjLTIzOTctNDAyYy1hYjZhLTFiNDcxMTE3N2FjMCJ9" frameborder="0" allowFullScreen="true"></iframe>
+            <iframe title="bi_incluses_terceiro_ano" width="80%" height="873.5" src="https://app.powerbi.com/view?r=eyJrIjoiYzYyODMxZjQtYWRkZi00YTY5LWE1YTQtOGM2ZTJkMzQ2ZWZiIiwidCI6ImIxNDhmMTRjLTIzOTctNDAyYy1hYjZhLTFiNDcxMTE3N2FjMCJ9" frameborder="0" allowFullScreen="true"></iframe>
             </div>
         </div>
     )
